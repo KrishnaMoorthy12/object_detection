9662da4e0b1f05d2ba654cba20c616c8554376f2
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-04-03 21:44:55 +0530

    update to release version and added a few beta version[debug]

--- object_detection-opencv-beta2.py
+++ object_detection-opencv-beta2.py
@@ -10,9 +10,9 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
-ap.add_argument("-p", "--prototxt", required=True,
+ap.add_argument("-p", "--prototxt", default = "MobileNetSSD_deploy.protxt.txt",
 	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
+ap.add_argument("-m", "--model", default = "MobileNetSSD_deploy.caffemodel",
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak detections")
@@ -26,7 +26,6 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 print("> Loading model from disk...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-
 image = cv2.imread(args["image"])
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
@@ -36,15 +35,17 @@ net.setInput(blob)
 detections = net.forward()
 
 print("> Calculating confidence levels...")
+k = 0
 for i in np.arange(0, detections.shape[2]):
 	confidence = detections[0, 0, i, 2]
 	if confidence > args["confidence"]:
+		k+=1
 		idx = int(detections[0, 0, i, 1])
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
 
 		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-		print("> Object: {}".format(label))
+		print("> Object {}: {}".format(j, label.capitalize()))
 		cv2.rectangle(image, (startX, startY), (endX, endY),
 			COLORS[idx], 2)
 		y = startY - 15 if startY - 15 > 15 else startY + 15
